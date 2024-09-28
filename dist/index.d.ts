6ebb1bd1dd087b1e801f2ab2097e1882e85f7588
keleris32 <charles.ejiegbu@gmail.com>
2024-09-28 11:06:24 +0100

    chore: transpiled to js

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,3 @@
-export * as TokenManager from './lib/jwtModule';
-export { codeGen } from './helpers/codeGen';
-export { successResponse } from './helpers/httpResponses';
-export { httpStatus } from "./helpers/httpStatus";
-export { Password } from "./lib/password";
-export { verifyToken } from "./middlewares/verifyToken";
-export { IdentityGenerator } from "./helpers/identityGenerator";
+export { codeGen, generateVerificationCode, getVerificationCodeExpiryDate, successResponse, errorResponse, httpStatus, IdentityGenerator, } from "./helpers";
+export { JwtModule, Password } from "./lib";
+export { verifyToken } from "./middlewares";
--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,19 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.IdentityGenerator = exports.verifyToken = exports.Password = exports.httpStatus = exports.successResponse = exports.codeGen = exports.TokenManager = void 0;
-exports.TokenManager = __importStar(require("./lib/jwtModule"));
-var codeGen_1 = require("./helpers/codeGen");
-Object.defineProperty(exports, "codeGen", { enumerable: true, get: function () { return codeGen_1.codeGen; } });
-var httpResponses_1 = require("./helpers/httpResponses");
-Object.defineProperty(exports, "successResponse", { enumerable: true, get: function () { return httpResponses_1.successResponse; } });
-var httpStatus_1 = require("./helpers/httpStatus");
-Object.defineProperty(exports, "httpStatus", { enumerable: true, get: function () { return httpStatus_1.httpStatus; } });
-var password_1 = require("./lib/password");
-Object.defineProperty(exports, "Password", { enumerable: true, get: function () { return password_1.Password; } });
-var verifyToken_1 = require("./middlewares/verifyToken");
-Object.defineProperty(exports, "verifyToken", { enumerable: true, get: function () { return verifyToken_1.verifyToken; } });
-var identityGenerator_1 = require("./helpers/identityGenerator");
-Object.defineProperty(exports, "IdentityGenerator", { enumerable: true, get: function () { return identityGenerator_1.IdentityGenerator; } });
+exports.verifyToken = exports.Password = exports.JwtModule = exports.IdentityGenerator = exports.httpStatus = exports.errorResponse = exports.successResponse = exports.getVerificationCodeExpiryDate = exports.generateVerificationCode = exports.codeGen = void 0;
+// Helpers
+var helpers_1 = require("./helpers");
+Object.defineProperty(exports, "codeGen", { enumerable: true, get: function () { return helpers_1.codeGen; } });
+Object.defineProperty(exports, "generateVerificationCode", { enumerable: true, get: function () { return helpers_1.generateVerificationCode; } });
+Object.defineProperty(exports, "getVerificationCodeExpiryDate", { enumerable: true, get: function () { return helpers_1.getVerificationCodeExpiryDate; } });
+Object.defineProperty(exports, "successResponse", { enumerable: true, get: function () { return helpers_1.successResponse; } });
+Object.defineProperty(exports, "errorResponse", { enumerable: true, get: function () { return helpers_1.errorResponse; } });
+Object.defineProperty(exports, "httpStatus", { enumerable: true, get: function () { return helpers_1.httpStatus; } });
+Object.defineProperty(exports, "IdentityGenerator", { enumerable: true, get: function () { return helpers_1.IdentityGenerator; } });
+// Lib
+var lib_1 = require("./lib");
+Object.defineProperty(exports, "JwtModule", { enumerable: true, get: function () { return lib_1.JwtModule; } });
+Object.defineProperty(exports, "Password", { enumerable: true, get: function () { return lib_1.Password; } });
+// Middlewares
+var middlewares_1 = require("./middlewares");
+Object.defineProperty(exports, "verifyToken", { enumerable: true, get: function () { return middlewares_1.verifyToken; } });
